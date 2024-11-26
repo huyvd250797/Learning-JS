51ebc87c9ef306007069e7d6dd8226ca6af023b1
Huy Vo <huyvd250797@gmail.com>
2024-11-26 22:04:02 +0700

    Learning-Javascript_Validate-I

--- learning-javascript_form-validationI/validator.js
+++ learning-javascript_form-validationI/validator.js
@@ -1,11 +1,69 @@
 //  Đối tượng `Validator`
 const Validator = (options) => {
-  // Khai báo biến get Element của form cần validate, form: "#form-1"
+  // Khai báo object rỗng để chứa các rules
+  let selectorRules = {};
+
+  //! Khai báo biến get Element của form cần validate, form: "#form-1"
   let formElement = document.querySelector(options.form);
 
   //? Nếu là formElement thì...
   if (formElement) {
+    // Khi submit form
+    formElement.onsubmit = function (e) {
+      e.preventDefault();
+
+      let isFormInvalid = true;
+
+      // Lặp qua các rule và vaidate
+      options.rules.forEach(function (rule) {
+        // và lấy inputElement
+        let inputElement = formElement.querySelector(rule.selector);
+        // và validate inputElement
+        let isValid = validate(inputElement, rule);
+
+        //? Nếu không có 1 phần tử không isValid
+        if (!isValid) {
+          // Gán isFromInvalid = false
+          isFormInvalid = false;
+        }
+      });
+
+      if (isFormInvalid) {
+        if (typeof options.onSubmit === "function") {
+          // Khai báo biến lấy tất cả input ở trạng thái enable
+          // Select tất cả field có attribute là name và không có attribute là disable
+          let enableInputs = formElement.querySelectorAll(
+            "[name]:not([disabled]"
+          );
+
+          // enableInputs đang là nodeList, Array.form sẽ biến enableInputs thành array
+          let formValues = Array.from(enableInputs).reduce(function (
+            values,
+            input
+          ) {
+            return (values[input.name] = input.value) && values;
+          },
+          {});
+
+          options.onSubmit(formValues);
+        }
+      }
+    };
+    //! Xử lý lặp qua mỗi rule và xử lý
     options.rules.forEach(function (rule) {
+      // Lưu lại các rules cho mỗi input
+
+      //? Nếu là mảng
+      if (Array.isArray(selectorRules[rule.selector])) {
+        // push rule.test vào mảng
+        selectorRules[rule.selector].push(rule.test);
+      }
+      //? Ngược lại không là mảng
+      else {
+        // gán key của object selectorRules = mảng với phần tử đầu tiên là rule.test
+        selectorRules[rule.selector] = [rule.test];
+      }
+
       // Không sử dụng document.querySelectorm, vì nếu trường hợp nhiều form cùng xài chung id sẽ lỗi
       // mà sử dụng luôn cái formElement đã khai báo ở trên để tìm chính xác form cụ thể
       let inputElement = formElement.querySelector(rule.selector);
@@ -14,8 +72,8 @@ const Validator = (options) => {
       if (inputElement) {
         // Tạo hàm lắng nghe sự kiện onblur khỏi input
         inputElement.onblur = function () {
-          //! value: inputElement.value
-          //! test func: rule.test
+          // value: inputElement.value
+          // test func: rule.test
           validate(inputElement, rule);
         };
 
@@ -44,7 +102,17 @@ const Validator = (options) => {
     );
 
     // Khai báo biến thông báo lỗi
-    let errMessage = rule.test(inputElement.value);
+    let errMessage;
+
+    // Khai báo biến chứa mảng selectorRules của input
+    let rules = selectorRules[rule.selector];
+
+    for (let i = 0; i < rules.length; i++) {
+      errMessage = rules[i](inputElement.value);
+
+      //? Nếu có thông báo lỗi break khỏi vòng lặp
+      if (errMessage) break;
+    }
     //? Nếu errMessage = true
     if (errMessage) {
       // Nếu có lỗi => hiển thị thông báo
@@ -61,6 +129,9 @@ const Validator = (options) => {
       // remove class border color
       inputElement.parentElement.classList.remove("invalid");
     }
+
+    // convert thành boolean thêm !! sẽ thành true & false
+    return !errMessage;
   };
 };
 
@@ -71,39 +142,54 @@ const Validator = (options) => {
 // Nguyên tác của rules:
 // 1. Khi có lỗi => trả mess lỗi
 // 2. Khi hợp lệ => không trả ra gì cả
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
   return {
     selector: selector,
     // Hàm thông báo lỗi
     test: function (value) {
       //? Nếu value is not null thì undefined, ngược lại thì báo lỗi
       // trim(): loại bỏ dấu cách để tránh người dùng nhập dấu cách vẫn tính hợp lệ
-      return value.trim() ? undefined : "Vui lòng nhập trường này";
+      return value.trim() ? undefined : message || "Vui lòng nhập trường này";
     },
   };
 };
 
 // Hàm kiểm tra người dùng đã nhập dữ liệu có phải là email không
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       // Kiểm tra nếu value người dùng nhập tuân thủ regex ? undefined : thông báo lỗi
-      return regex.test(value) ? undefined : "Vui lòng nhập email hợp lệ";
+      return regex.test(value)
+        ? undefined
+        : message || "Vui lòng nhập email hợp lệ";
     },
   };
 };
 
 // Hàm kiểm tra độ dài password người dùng nhập
-Validator.minLength = function (selector, min) {
+Validator.minLength = function (selector, min, message) {
   return {
     selector: selector,
     test: function (value) {
       // Kiểm tra nếu value người dùng nhập tuân thủ regex ? undefined : thông báo lỗi
-      return value.lenght >= min
+      return value.length >= min
         ? undefined
-        : `Vui lòng nhập tối thiểu ${min} ký tự`;
+        : message || `Vui lòng nhập tối thiểu ${min} ký tự`;
+    },
+  };
+};
+
+// Hàm confirm password
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value == getConfirmValue()
+        ? undefined
+        : // Nếu có tồn tại message thì lấy message nếu không thì lấy "Giá trị xác nhận không khớp"
+          message || "Giá trị xác nhận không khớp";
     },
   };
 };
